24f9d0118c0a1021d8e5151d69ecfb9ec7c6e6f7
Ayushi <ayushiverma844@gmail.com>
2026-07-07 21:41:43 +0530

    navbar updated

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,71 +3,24 @@ import { Link, useNavigate } from "react-router-dom";
 
 
 import {
-  Search,
+  Landmark ,
   User,
-  Bell,
   House,
   Compass,
   BookOpenText,
-  Heart,
   Menu,
   X,
   Bookmark,
 } from "lucide-react";
 
-const notifications = [
-  {
-    id: 1,
-    title: "New Story Added",
-    message: "The Legend of Krishna is now available.",
-    time: "2h ago",
-    icon: "✨",
-  },
-  {
-    id: 2,
-    title: "New Place Added",
-    message: "Amer Fort has been added.",
-    time: "5h ago",
-    icon: "🏛",
-  },
-  {
-    id: 3,
-    title: "Favorite Update",
-    message: "Hawa Mahal received 120 new reviews.",
-    time: "1 day ago",
-    icon: "❤️",
-  },
-];
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-const [showNotifications, setShowNotifications] = useState(false);
-const notificationRef = useRef(null);
 const navigate = useNavigate();
 
 const [user, setUser] = useState(null);
-useEffect(() => {
-  const handleClickOutside = (event) => {
-    if (
-      notificationRef.current &&
-      !notificationRef.current.contains(event.target)
-    ) {
-      setShowNotifications(false);
-    }
-  };
 
-  document.addEventListener(
-    "mousedown",
-    handleClickOutside
-  );
-
-  return () => {
-    document.removeEventListener(
-      "mousedown",
-      handleClickOutside
-    );
-  };
-}, []);
 
 useEffect(() => {
 
@@ -86,7 +39,7 @@ const handleLogout = () => {
 
   setUser(null);
    setIsOpen(false);
-  setShowNotifications(false);
+  
 
   navigate("/");
 
@@ -146,134 +99,10 @@ const handleLogout = () => {
 
         {/* Right Section */}
        <div
-  ref={notificationRef}
-  className="relative flex items-center gap-2 md:gap-4"
+  className="relative flex items-center gap-2 md:gap-5"
 >
           
-          {/* Desktop notification */}
-  {user && (        
-           <button
-  onClick={() =>
-    setShowNotifications(!showNotifications)
-  }
-  className="
-  relative
-  hidden
-  md:flex
-  h-10
-  w-10
-  rounded-full
-  bg-white/5
-  border
-  border-white/10
-  items-center
-  justify-center
-  text-white
-  hover:text-heritage-gold
-  hover:scale-110
-  transition-all
-  duration-300
-  
-  "
->
-  <Bell size={20} />
-
-  {/* Notification Dot */}
-  <span
-    className="
-    absolute
-    top-2
-    right-2
-    h-2
-    w-2
-    rounded-full
-    bg-red-500
-    animate-pulse
-    "
-  />
-</button>
-)}
-{user && showNotifications && (
-  <div
-    className="
-    absolute
-    top-15
-    right-0
-    w-85
-    rounded-3xl
-    bg-heritage-dark/50
-    backdrop-blur-xl
-    border
-    border-white/10
-    shadow-2xl
-    overflow-hidden
-    animate-in fade-in zoom-in-95 duration-200
-    "
-  >
-    <div className="p-5 border-b border-white/10">
-
-      <h3 className="text-lg font-semibold text-white">
-        Notifications
-      </h3>
-
-    </div>
-
-    <div className="max-h-96 overflow-y-auto">
-
-      {notifications.map((item) => (
-        <div
-          key={item.id}
-          className="
-          p-4
-          border-b
-          border-white/5
-          hover:bg-white/5
-          transition-all
-          cursor-pointer
-          "
-        >
-          <div className="flex gap-3">
-
-            <div className="text-xl">
-              {item.icon}
-            </div>
-
-            <div>
-
-              <h4 className="font-medium text-white">
-                {item.title}
-              </h4>
-
-              <p className="text-sm text-gray-400 mt-1">
-                {item.message}
-              </p>
-
-              <span className="text-xs text-gray-500">
-                {item.time}
-              </span>
-
-            </div>
-
-          </div>
-        </div>
-      ))}
-
-    </div>
-
-    <button
-      className="
-      w-full
-      py-4
-      text-center
-      text-heritage-gold
-      hover:bg-white/5
-      transition-all
-      "
-    >
-      View All Notifications
-    </button>
-  </div>
-)} 
+ 
 
 
 
@@ -331,17 +160,26 @@ Login
 {user?.role === "admin" && (
 
 <Link
-
 to="/admin"
-
-onClick={() => setIsOpen(false)}
-
-className="flex items-center gap-3 text-gray-300 hover:text-heritage-gold"
-
+className="
+hidden md:flex
+items-center
+gap-2
+px-4
+py-2
+rounded-xl
+bg-heritage-gold/10
+border
+border-heritage-gold/20
+text-heritage-gold
+hover:bg-heritage-gold
+hover:text-black
+transition-all
+duration-300
+"
 >
-
+<Landmark size={16}/>
 Dashboard
-
 </Link>
 
 )}
