554d97309de206412f8864ebc6d2ad9208ee4d4a
Ayushi <ayushiverma844@gmail.com>
2026-07-19 17:46:13 +0530

    adding db_port

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2/promise");
 
 const MyConnection = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -14,10 +15,10 @@ const MyConnection = mysql.createPool({
 (async () => {
   try {
     const connection = await MyConnection.getConnection();
-    console.log("Connected to MySQL Database");
+    console.log("✅ Connected to MySQL Database");
     connection.release();
   } catch (error) {
-    console.error("Database Connection Failed:", error);
+    console.error("❌ Database Connection Failed:", error);
   }
 })();
 
